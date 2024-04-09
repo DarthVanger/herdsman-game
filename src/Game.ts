328d7737be420ae91c47f991ecdd953c2f10402d
DarthVanger <darthvanger@gmail.com>
2024-04-09 14:36:23 +0300

    Clean up Game.ts

--- src/Game.ts
+++ src/Game.ts
@@ -1,5 +1,5 @@
-import { Text, type Ticker } from 'pixi.js'
-import { pixiApp } from './pixiApp'
+import { type Container, Text, type Ticker } from 'pixi.js'
+import { getGameDimensions, pixiApp } from './pixiApp'
 import { spawner } from './Spawner'
 import { type System } from './ecsFramework/System'
 import { MoveToClickPositionSystem } from './systems/MoveToClickPositionSystem'
@@ -16,6 +16,7 @@ import { RenderSystem } from './systems/RenderSystem'
 export class Game {
   htmlElement: HTMLElement
   systems: System[]
+  loader: Container
 
   async bootstrap (): Promise<void> {
     await pixiApp.init({ background: 'black', resizeTo: window })
@@ -35,10 +36,11 @@ export class Game {
       new IsInsideAreaSystem()
     ]
 
-    const loadingGraphics = new Text('Loading...', { fontFamily: 'Arial', fontSize: 24, fill: 0xff1010, align: 'center' })
-    pixiApp.stage.addChild(loadingGraphics)
+    this.showLoader()
 
     await assetManager.preload()
+
+    this.removeLoader()
 
     spawner.spawnGameField()
     spawner.spawnYard()
@@ -48,8 +50,6 @@ export class Game {
 
     await Promise.all(this.systems.map(async system => { await system.setup?.() }))
 
-    pixiApp.stage.removeChild(loadingGraphics)
-
     pixiApp.ticker.add(time => { this.gameLoop(time) })
   }
 
@@ -57,5 +57,24 @@ export class Game {
     for (const system of this.systems) {
       system.update?.(time.deltaTime)
     }
+  }
+
+  private showLoader (): void {
+    this.loader = new Text(
+      {
+        text: 'Loading...',
+        style: {
+          fontFamily: 'Arial', fontSize: 24, fill: 0xff1010, align: 'center'
+        }
+      }
+    )
+    this.loader.x = getGameDimensions().width / 2 - this.loader.width / 2
+    this.loader.y = getGameDimensions().height / 2 - this.loader.height / 2
+
+    pixiApp.stage.addChild(this.loader)
+  }
+
+  private removeLoader (): void {
+    pixiApp.stage.removeChild(this.loader)
   }
 }
